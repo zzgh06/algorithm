030e3da28c7b99c17eca0f970636abdf3062b792
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-01-18 14:40:04 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,12 +1,7 @@
-s = int(input())
+n = int(input())
 
-if s >= 90:
-    print('A')
-elif s >= 80:
-    print('B')
-elif s >= 70:
-    print('C')
-elif s >= 60:
-    print('D')
-else:
-    print('F')
+if n >= 90: print("A")
+elif n >= 80: print("B")
+elif n >= 70: print("C")
+elif n >= 60: print("D")
+else: print("F")
