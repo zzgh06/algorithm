4e61b1afd8ec97b976f374871ecc69d5c4f88327
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-01-21 18:25:33 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,6 @@
-students = [i for i in range(1,31)]
+num = [i for i in range(1, 31)]
 
-for n in range(28):
-    num = int(input())
-    students.remove(num)
-
-print(min(students))
-print(max(students))
+for s in range(28):
+    n = int(input())
+    num.remove(n)
+print(*num, sep='\n')
