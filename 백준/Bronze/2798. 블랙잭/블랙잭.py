5071f5d4212f0d5c0ae172c24e1174c70d6cc8c5
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-02-01 14:29:01 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+max_total = 0
+
+for i in range(n-2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            total = cards[i] + cards[j] + cards[k]
+            if total <= m:
+                max_total = max(max_total, total)
+
+print(max_total)
