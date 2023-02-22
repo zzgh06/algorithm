b7f6538ccf8581168595eb04bbfd1b33b5afe1f0
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-02-22 22:15:59 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 120 ms, Memory: 37032 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,14 +1,20 @@
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
+import sys
+input = sys.stdin.readline
 
-# 부분의 개수 입력
-K = int(input())
-dp = [[0] * (M+1) for _ in range(N+1)]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+# print(arr) [[1, 2, 4], [8, 16, 32]]
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        dp[i][j] = arr[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
+# 부분의 개수
+k = int(input())
+dp = [[0] * (m+1) for _ in range(n+1)]
+# print(dp) [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
-for _ in range(K):
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = arr[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] # 2차원 배열 누적합 공식 [[0, 0, 0, 0], [0, 1, 3, 7], [0, 9, 27, 63]]
+
+for _ in range(k):
     i, j, x, y = map(int, input().split())
     print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[i-1][j-1])
