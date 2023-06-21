ae8766bc39765a40cb05ac5b3273286182161457
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-06-21 11:30:42 +0900

    [Bronze II] Title: 분해합, Time: 1208 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,11 @@
 n = int(input())
-total = 0
-for i in range(1, n +1):
-    m = list(map(int, str(i)))
-    total = i + sum(m)
+
+for i in range(1, n+1):
+    num = sum(map(int, str(i)))
+    total = i + num # 분해합 = 생성자 + 각 자릿수의 합
+    # i가 작은 수부터 차례로 들어가므로 처음으로 분해합과 입력값이 같을때가 가장 작은 생성자를 가짐
     if total == n:
         print(i)
         break
-    if i == n:
+    elif i == n: # 생성자 i와 입력값이 같다는 것은 생성자가 없다는 뜻
         print(0)
