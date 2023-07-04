46c1c55ea122c60d2c490053d67c0430e6b12fa3
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-07-04 12:08:27 +0900

    [Silver IV] Title: 듣보잡, Time: 3800 ms, Memory: 44060 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -2,13 +2,14 @@ n, m = map(int, input().split())
 a = set()
 b = set()
 
-for _ in range(n):
-    a.add(input()) # set에 요소를 추가할 때는 add 메서드 사용
-for _ in range(m):
+for i in range(n):
+    a.add(input())
+
+for i in range(m):
     b.add(input())
 
-result = sorted(list(a & b)) # 교집합을 통해 중복된 값을 변수 저장
-print(len(result))
+res = sorted(list(a & b))
+print(len(res))
 
-for i in result:
+for i in res:
     print(i)
