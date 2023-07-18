a8e616f71864917ad277709cb41fd29a300e7b2a
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-07-18 19:11:54 +0900

    [Bronze I] Title: 세로읽기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,6 +1,12 @@
-matrix = [input() for i in range(5)]
+arr = [[] for _ in range(15)]
 
-for j in range(15): # 최대 15개의 글자들이 빈칸 없이 연속으로 주어짐
-    for i in range(5): 
-        if j < len(matrix[i]): # 조건문에 의해 j가 0 ~ 4 행의 범위만큼(?) 출력되게 함
-            print(matrix[i][j], end = '')
+for _ in range(5):
+    words = input()
+    for i in range(len(words)):
+        arr[i].append(words[i])
+
+result = ''
+
+for i in range(len(arr)):
+    result += ''.join(arr[i])
+print(result)
