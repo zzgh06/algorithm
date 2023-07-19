135b153b8c46ca499f92c07016ad9552a68bae25
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-07-19 18:23:04 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,13 +1,9 @@
-students = []
+n = int(input())
+information = [ list(map(int, input().split())) for _ in range(n) ]
 
-for _ in range(int(input())):
-    x, y = map(int, input().split())
-    students.append((x, y))  # 튜플의 형태로 저장
-
-for i in students:
-    rank = 1  # 더 덩치 큰 사람이 k명이라면 k+1이기 때문에 1부터 비교
-    for j in students:
-        if i[0] < j[0] and i[1] < j[1]:  # 0번째 튜플 값과 0~4번째 튜플 값을 순회하며 비교하고 더 클 경우
-            rank += 1  # rank 변수에 1를 더한다
-
-    print(rank, end=' ')
+for i in information:
+    rank = 1
+    for j in information:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=" ")
