d18e4e2bf06d894d751150aa27367e63e05f0e8c
chunghyun <121420299+zzgh06@users.noreply.github.com>
2023-07-19 12:30:36 +0900

    [Bronze III] Title: 나는 요리사다, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.py
@@ -1,3 +1,10 @@
-matrix = [sum(list(map(int, input().split()))) for _ in range(5)]
+score = [ list(map(int, input().split())) for _ in range(5) ]
+score_total = []
 
-print(matrix.index(max(matrix)) + 1, max(matrix))
+for i in range(5):
+    total = 0
+    for j in range(4):
+        total += score[i][j]
+    score_total.append(total)
+
+print(score_total.index(max(score_total)) + 1, max(score_total))
