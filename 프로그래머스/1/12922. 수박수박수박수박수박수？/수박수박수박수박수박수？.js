15fc2d62feff74971922ab5c883b531e7f9b6c9c
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-09 11:47:57 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = '';
+    for (let i = 0; i < n; i++){
+        if (i%2 == 1) {
+            answer += '박';
+        } else {
+            answer += '수';
+        }
+    }
+    return answer
+}
