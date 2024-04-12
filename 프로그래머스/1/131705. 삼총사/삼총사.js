d9e457947cae0a3249667d69ebf30771cb804a65
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-12 11:53:17 +0900

    [level 1] Title: 삼총사, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,13 +1,13 @@
 function solution(number) {
     var answer = 0;
-    for (let i = 0; i < number.length; i++){
-        for (let j = 0; j < number.length; j++){
-            for (let k = 0; k < number.length; k++)
-                if(i !== j && j !== k && i !== k){
-                    if((number[i] + number[j] + number[k])===0)
-                    answer += 1
+    for(var i=0; i<number.length; i++){
+        for(var j=i+1; j<number.length; j++){
+            for(var k=j+1; k<number.length; k++){
+                if(number[i] + number[j] + number[k] === 0){
+                    answer += 1;
                 }
+            }
         }
     }
-    return answer/6;
+    return answer;
 }
