5ad00583a209220c3180fe157fb0c5b6d9b1bcd7
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-17 15:22:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,20 +1,21 @@
 function solution(n, arr1, arr2) {
-    let answer = [];
-    for(let i = 0; i < n; i++) {
-        // 비트 연산자 OR a|b 대응되는 비트 중 하나라도 1이면 1를 반환히고 이진수로 변환
-        let binaryNumber = (arr1[i] | arr2[i]).toString(2)
+    const answer = [];
+    // 비트연산자를 통해 각 배열을 이진수로 변환
+    for (let i = 0; i < n; i++){
+        const binaryNumber = (arr1[i] | arr2[i]).toString(2);
+        // console.log(binaryNumber)   
         answer.push(binaryNumber)
     }
     
-    // answer의 배열 값의 길이가 n보다 작을경우 0를 붙여준다
-    for(let i = 0; i < n; i++) {
-    while(answer[i].length < n) {
+    // 배열의 담긴 이진수의 길이가 n보다 작을 경우, 앞에 0를 추가
+    for (let i = 0; i < n; i++){
+        while (answer[i].length < n){
             answer[i] = '0' + answer[i]
         }
     }
     
-    // 배열의 값 중 1은 #으로 0은 " " 공백으로 치환
-    for(let i = 0; i < n; i++) {
+    // 정규표현식을 통해 1의 경우 #으로 0은 공백으로 치환
+    for (let i = 0; i < n; i++){
         answer[i] = answer[i].replace(/1/g, '#')
         answer[i] = answer[i].replace(/0/g, ' ')
     }
