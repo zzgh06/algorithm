01e198993ac448b22fe34752fe01b836eb5874f4
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-17 15:29:50 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,11 +1,10 @@
 function solution(a, b, n) {
-    let answer = 0
-    while (n >= a) {
-        // 바꿀 수 있는 빈 병 수 * 교환받는 콜라수 b
-        answer += Math.floor(n / a) * b; 
-        // 바꿀 수 있는 빈 병 수(교환하고 먹으면 빈 병) + 교환하고 남은 빈 병
-        // 딱 나누어지지 않을 수 있기 때문에 바꿀 수 있는 빈 병 수 + 교환하고 남은 빈 병 해준다
-        n = (Math.floor(n / a) * b) + n % a; 
+    let answer = 0;
+    while (n >= a){
+        // 1. 교환 가능한 콜라 수 구하기
+        answer += Math.floor(n/a) * b
+        // 2. 얻은 빈 병 갯수 구하기
+        n = (Math.floor(n/a) * b) + n % a;
     }
-    return answer
+    return answer;
 }
