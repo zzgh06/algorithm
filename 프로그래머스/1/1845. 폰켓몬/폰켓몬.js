e4f60172968df7e6065d8130b44f4c098145ac38
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-19 11:42:25 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,7 @@
+// 대한 다양한 종류의 폰켓몬을 가지길 원하기 때문에 배열에서 중복값 제거
+// 최대 N/2마리를 가져가야 함 
 function solution(nums) {
-    let answer = [...new Set(nums)]; // 
-    let limit = nums.length / 2;
-
-    return answer.length > limit ? limit : answer.length
+    let monster = [...new Set(nums)];
+    let maxChoice = nums.length / 2;
+    return monster.length > maxChoice ? maxChoice : monster.length;
 }
