cd84840714bd0004cc4ab706cda8dc5d300ffb8e
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-22 10:31:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.64 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,29 +1,27 @@
-// 1. 해시 테이블 생성
-// 2. 참가자 이름 세기 key(이름) value 1 (동명이인이 있으면 value +1)
-// 3. 완주자 이름 세기 key(이름) value 1 (동명이인이 있으면 value -1)
-// 4. value 1만 출력 (완주하였으면 value 0 완주하지 못하였으면 value 1) >>> for of 문 활용
+// completion의 길이는 participant의 길이보다 1 작으므로
+// participant 배열의 값을 key로 value를 인원수로 해서 hash에 넣고
+// hash에서 completion의 배열을 key 값으로 이용해서 일치할 경우 -1
 function solution(participant, completion) {
     let answer = ''
     let hashMap = new Map();
     
     participant.forEach((participant) => {
-        if (hashMap.get(participant)){
+        if (hashMap.get(participant)) {
             hashMap.set(participant, hashMap.get(participant) + 1)
         } else {
             hashMap.set(participant, 1)
         }
     })
-    
+    // console.log(hashMap.values())
     completion.forEach((completion) => {
-        if (hashMap.get(completion)){
+        if (hashMap.get(completion)) {
             hashMap.set(completion, hashMap.get(completion) - 1)
         }
     })
-    
-    for (const [key, value] of hashMap) {
-        if (value >= 1) {
-            answer += key
+    // console.log(hashMap.values())
+    for (const [key, value] of hashMap){
+        if (value > 0) {
+            return key
         }
     }
-    return answer
 }
