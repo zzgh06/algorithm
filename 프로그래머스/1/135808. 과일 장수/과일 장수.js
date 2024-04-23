d8ba862ca6fdb8796c9f0817f0ccf5bd2fcc4ed0
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-23 10:30:17 +0900

    [level 1] Title: 과일 장수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -3,14 +3,15 @@
 // 상자에 담긴 사과 중 가장 낮은 점수가 p (1 ≤ p ≤ k)점인 경우, 사과 한 상자의 가격은 p * m 입니다.
 // 가능한 많은 사과를 팔았을 때, 얻을 수 있는 최대 이익을 계산하고자 합니다
 
-// score 높은 순으로 정렬하여 m의 길이마다 그 중 가장 낮은 값과 * m 하여 값을 구한다
-
+// ▼아이디어를 적고 풀어보기▼
+// score를 내림차순으로 정렬(최대이익)
+// m개씩 나누어 그중 가장 낮은 p(m개 중 마지막 요소) * m 하여 answer에 더해간다
 function solution(k, m, score) {
     let answer = 0;
-    let temp = score.sort((a,b) => (b - a))
-    // console.log(temp) [3, 3, 2, 2, 1, 1, 1] [4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 1, 1]
-    for (let i = m; i <= temp.length; i = i + m){
-        answer += temp[i - 1] * m
+    score = score.sort((a, b) => b-a) 
+    // console.log(score)  [3, 3, 2, 2, 1, 1, 1]
+    for (let i = m; i <= score.length; i = i + m) {
+        answer += score[i - 1] * m
     }
     return answer
 }
