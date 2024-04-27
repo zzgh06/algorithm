c5719f13cd97b28bf155c621183a5d02a790d700
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-04-27 22:55:11 +0900

    [level 1] Title: 실패율, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,42 +1,37 @@
+// 1. hashMap 생성
+// 2. 1스테이지부터 n스테이지까지 순회
+// 3. 스테이지 통과한 사람 구하기
+// 4. 스테이지 통과하지 못한 사람 구하기
+// 5. 실패율 구하기
+// 6. 해당 스테이지와 실패율을 key value 로 저장
+// 7. 실패율에 따른 내림차순 정렬
+// 8. 내림차순 정렬된 key 값만 출력
 function solution(N, stages) {
-    let answer = [];
-    // 1)
+    var answer = [];
     let hashMap = new Map();
-
-    // 2)
-    for(let i=1; i<=N; i++) {
-        let stage_clear = 0;
-        let stage_fail = 0;
-        let failure_rage = 0;
-
-        // 3)
-        for(let j=0; j<stages.length; j++) {
-            if(i <= stages[j]) { 
-                stage_clear += 1;
+    
+    // 스테이지 단계
+    for (let i = 1; i <= N; i++) {
+        let clear = 0;
+        let fail = 0;
+        let failureRate = 0;
+        for (let j = 0; j < stages.length; j++){
+            if (i <= stages[j]) {
+                clear += 1;
             }
         }
-
-        // 4)
-        for(let k=0; k<stages.length; k++) {
+        for(let k = 0; k < stages.length; k++) {
             if(stages[k] == i) {
-                stage_fail += 1;
+                fail += 1;
             }
         }
-
-        // 5)
-        failure_rage = stage_fail / stage_clear
-
-        // 6)
-        hashMap.set(i, failure_rage)
+        failureRate = fail / clear;
+        hashMap.set(i, failureRate)
     }
-
-    // 7)
-    // Map(5) {1 => 0.125, 2 => 0.42857142857142855, 3 => 0.5, 4 => 0.5, 5 => 0}
-    let sortedArr = [...hashMap].sort((a, b) => b[1] - a[1]); // Map 객체를 배열로 변환 후, 내림차순으로 정렬
-
-    // 8)
-    // sortedArr = [[ 3, 0.5 ], [ 4, 0.5 ], [ 2, 0.42857142857142855 ], [ 1, 0.125 ], [ 5, 0 ]];
-    answer = sortedArr.map(e => e[0])
-
-    return answer;
+    // 정렬 시 value을 기준으로 정렬
+    let sortedHashMap = [...hashMap].sort((a, b) => b[1] - a[1]);
+    
+    // 내림차순 정렬된 key 값만 출력
+    answer = sortedHashMap.map(hash => hash[0])
+    return answer
 }
