ab06fc85f241cbe7820476d74c456a8e2958ea1f
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-03 14:27:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,22 +1,13 @@
 function solution(n, arr1, arr2) {
     const answer = [];
     // 각 배열을 비트 연산자를 통해 이진수로 변환
-    for (let i = 0; i < n; i++){
+    for (let i = 0; i < n; i ++){
         let binaryNumber = (arr1[i] | arr2[i]).toString(2);
         answer.push(binaryNumber)
     }
-    
-    // 9의 경우 이진수는 1001인데 n(=5)의 자릿수보다 부족하다. 
-    // 배열에 담긴 이진수의 길이가 n보다 작을 경우 앞에 0를 붙이자     
-    for (let i = 0; i < n; i++){
-        while (answer[i].length < n) {
-            answer[i] = '0' + answer[i] 
-        }
-    }
-    
-    // 정규표현식을 이용해서 1 은 #으로 0은 공백으로 처리
-    for (let i = 0; i < n; i++){
-        answer[i] = answer[i].replace(/1/g, '#').replace(/0/g, ' ')
-    }
-    return answer
+    console.log(answer)
+    // map()으로 순환하여 정규식을 사용하여 1은 #으로 0은 ' '공백으로 변환 후 반환
+    // 2진법 변환 중 앞에 0이 사라지는 경우가 존재하기 때문에
+    // padStart()를 이용하여 n보다 작을 경우, 앞에 0를 붙여준다
+    return answer.map((ele) => ele.padStart(n, '0').replace(/1/g, '#').replace(/0/g, ' '))
 }
