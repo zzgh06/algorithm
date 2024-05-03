da037abbdb282ed7a4fbc19001286dc76529a342
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-03 13:55:47 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -10,7 +10,7 @@ function solution(dartResult) {
     
     for (let i = 0; i < dartResult.length; i++) {
         // 1) 점수는 0점에서 10점까지(10인 경우는 예외처리)
-        if (!isNaN(dartResult[i])){ // i번째가 숫자일때(10인 경우에만 i번째가 숫자이니깐)
+        if (!isNaN(dartResult[i])){ 
             temp = Number(dartResult[i-1]) === 1 ? 10 : Number(dartResult[i])
         }
         // 2) S ** 1, D ** 2, T ** 3
@@ -23,16 +23,15 @@ function solution(dartResult) {
         else if (dartResult[i] === 'T') {
             score.push(Math.pow(temp, 3))
         }
-        // 4) 스타상(*)의 효과는 다른 스타상(*)의 효과와 중첩
+        // 3) 스타상(*) 당첨 시 * 2 / 아차상(#) 당첨 시 해당 점수는 마이너스
         else if (dartResult[i] === '*') {
             score[score.length - 2] *= 2
             score[score.length - 1] *= 2
         }
-        // 5) 아차상(#)의 효과
-        else if (dartResult[i] === "#") {
-          score[score.length - 1] *= -1 
+        else if (dartResult[i] === '#') {
+            score[score.length - 1] *= -1
         }
     }
-    answer = score.reduce((arr, cur)=>  arr + cur, 0)
+    answer = score.reduce((prev, cur) => prev + cur, 0)
     return answer
 }
