676479a000c92fcd21dfcd249a4b87095158a893
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-13 11:30:37 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,17 +1,16 @@
 function solution(num) {
     let cnt = 0
-    while (num != 1){
-        if (cnt == 500){
-            cnt = -1;
-            break;
+    while(num !== 1) {
+        if (cnt === 500){
+            cnt = -1
+            break
         }
-        
         if (num%2 == 0){
             num = num/2
         } else {
             num = num*3 +1 
         }
-            cnt++;
+            cnt += 1
     }
     return cnt
 }
