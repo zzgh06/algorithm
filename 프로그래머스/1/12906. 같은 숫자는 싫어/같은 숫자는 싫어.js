e2b9db45fcec27c2e61544a829a92b2415edbbbf
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-14 12:26:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.12 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,14 @@
-function solution(arr)
-{
-    let beforeNumber
-    var answer = [];
-    arr.forEach((num)=>{
-        if (beforeNumber !== num){
-            answer.push(num)
+// stack 자료 구조
+// arr 배열의 요소를 하나 빼서 새로운 배열에 넣고, 이전의 넣은 값과 비교해서
+// 같은 숫자인 경우 제거 아니면 그냥 넘김
+function solution(arr) {
+    let newArr = [[...arr][0]];
+    // console.log(newArr)
+    for (let i = 0; i < arr.length - 1; i++){
+        // console.log(arr[i], arr[i + 1])
+        if (newArr[newArr.length - 1] !== arr[i+1]) {
+            newArr.push(arr[i+1])
         }
-        beforeNumber = num
-    })
-    return answer
+    }
+    return newArr
 }
