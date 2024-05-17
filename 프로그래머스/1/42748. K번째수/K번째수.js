315b4bb8e1e1126ebb0e435e89e192e8d7c0203b
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-17 12:38:39 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,9 @@
 function solution(array, commands) {
-    const result = []
-    commands.map((nums)=>{
-        // console.log(nums) // [ 2, 5, 3 ] [ 4, 4, 1 ] [ 1, 7, 3 ]
-        const arr = array.slice(nums[0] - 1, nums[1]).sort((a, b) => a - b)
-        const idx = nums[2] - 1
-        // console.log(idx) // 3 1 3
-        // console.log(arr) // [ 2, 3, 5, 6 ] [ 6 ] [1, 2, 3, 4, 5, 6, 7]
-        result.push(arr[idx])
+    let res = []
+    commands.map((command)=>{
+        // console.log(command)
+        let arr = array.slice(command[0]-1, command[1]).sort((a, b) => a-b)
+        res.push(arr[command[2] - 1])
     })
-    return result
+    return res
 }
