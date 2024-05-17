14e03b2bccadb1bef0a799d4fee817ff032d5a0c
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-17 13:04:32 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,15 +1,15 @@
+// 점수를 내림차순으로 정렬
+// k번째 이상부터 끝의 값을 제거(내림차순이기 때문에 배열의 끝의 값이 가장 작은 값)
+// answer에 각 첫번쩨 값을 배열에 담아서 리턴
+// score.sort((a, b)=> b - a)
 function solution(k, score) {
-    var answer = [];
-    return score.map((num)=>{
-        answer.push(num)
-        // 점수를 내림차순으로 정렬
-        answer.sort((a, b) => b - a)
-        
-        // k번째 이상부터 끝의 값을 제거(내림차순이기 때문에 배열의 끝의 값이 가장 작은 값)
-        if (answer.length > k) {
+    let answer = [];
+    return score.map((score)=>{
+        answer.push(score)
+        answer.sort((a,b)=>b-a)
+        if (answer.length > k){
             answer.pop()
         }
-        // answer에 각 첫번쩨 값을 배열에 담아서 리턴
-        return answer[answer.length - 1];  
+        return answer[answer.length - 1]
     })
 }
