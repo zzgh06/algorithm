6708fc1dc28ad211767b1be24b3131d63bcae64b
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-19 22:57:55 +0900

    [level 1] Title: 모의고사, Time: 1.44 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,34 +1,22 @@
-// 각 학생의 문제를 찍는 패턴을 저장
-// 각 패턴이 answers 배열의 정답과 일치할 경우 점수 추가
+// 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌으므로
+// 각 학생이 문제를 찍은 패턴과 answers가 일치할수록 높은 점수를 얻을 수 있음
 function solution(answers) {
     let result = [];
-    let score1 = 0, score2 = 0, score3  = 0;
+    let score1 = 0, score2 = 0, score3 = 0; 
     
     let student1 = [1, 2, 3, 4, 5]
     let student2 = [2, 1, 2, 3, 2, 4, 2, 5]
     let student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    answers.forEach((ans, idx) =>{
-        if (ans === student1[idx % student1.length]){
-            score1++;
-        }
-        if (ans === student2[idx % student2.length]){
-            score2++;
-        }
-        if (ans === student3[idx % student3.length]){
-            score3++;
-        }
+    answers.forEach((ans, idx)=>{
+        if (ans === student1[idx % student1.length]){score1++}
+        if (ans === student2[idx % student2.length]){score2++}
+        if (ans === student3[idx % student3.length]){score3++}
+        // console.log(score1, score2, score3)
     })
     const maxScore = Math.max(score1, score2, score3)
-    
-    if (score1 === maxScore) {
-        result.push(1)
-    }
-    if (score2 === maxScore) {
-        result.push(2)
-    }
-    if (score3 === maxScore) {
-        result.push(3)
-    }
+    if (maxScore === score1){result.push(1)}
+    if (maxScore === score2){result.push(2)}
+    if (maxScore === score3){result.push(3)}
     return result
 }
