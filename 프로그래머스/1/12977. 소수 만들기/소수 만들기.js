ab043798ff1278f7eb3865affe762ab49ee22e20
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-21 12:15:29 +0900

    [level 1] Title: 소수 만들기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -3,25 +3,22 @@
 // 3. 소수 판별
 
 function solution(nums) {
-    let answer = 0;
-    let arr = [];
-    
-    // 1. 주어진 숫자 중 서로 다른 3개를 뽑는다.
+    let res = 0
+    let arr = []
     for (let i = 0; i < nums.length; i++){
-        for (let j = i+1; j < nums.length; j++){
-            for (let k = j+1; k < nums.length; k++){
-                // 2. 서로 다른 3개를 골라 더한다. 
-                arr.push(nums[i] + nums[j] + nums[k])
-            }
+        for (let j = i + 1; j < nums.length; j++){
+            for (let k = j + 1; k < nums.length; k++)
+                // console.log(i, j, k)
+                arr.push(nums[i] + nums[j] + nums[k]);
         }
     }
-    // 3. 소수 판별
-    arr.forEach((num)=>{
+    arr.forEach(num => {
+        // console.log(num)
         let decimal = true;
-        for(let i = 2; i <= Math.sqrt(num); i++) {
-            if(num % i === 0) decimal = false;
+        for (let i = 2; i * i <= num; i++){
+            if (num%i === 0) decimal = false;
         }
-        answer += (decimal ? 1 : 0)
+        res += decimal ? 1 : 0
     })
-    return answer
+    return res
 }
