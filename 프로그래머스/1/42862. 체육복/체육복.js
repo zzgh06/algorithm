22b1b2ae501e4b986fa0c4f99c0ea5bb0dde4b1c
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-05-29 12:07:16 +0900

    [level 1] Title: 체육복, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -0,0 +1,19 @@
+// answer = n - lost.length;
+// lost 앞뒤에 reverse 가 있는지
+// 있고 해당이 되면 ++, 없으면 --
+// 주의 : 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다
+function solution(n, lost, reserve) {
+    // 여벌없이 분실한 학생
+    let lost_student = lost.filter(l => !reserve.includes(l)).sort((a, b) => a - b);
+    // 분실없이 여벌이 있는 학생
+    let reserve_student = reserve.filter(r => !lost.includes(r)).sort((a, b) => a - b);
+    
+    for (let i = 0; i < reserve_student.length; i++) {
+        if (lost_student.includes(reserve_student[i] - 1)) {
+            lost_student = lost_student.filter(v => v !== reserve_student[i] - 1);
+        } else if (lost_student.includes(reserve_student[i] + 1)) {
+            lost_student = lost_student.filter(v => v !== reserve_student[i] + 1);
+        }
+    }
+    return n - lost_student.length;
+}
