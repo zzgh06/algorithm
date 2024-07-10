6c62a33527bd70820534c988a18823ed8c324852
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-07-10 13:39:35 +0900

    [level 2] Title: 짝지어 제거하기, Time: 23.54 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,15 +1,14 @@
-// s.length === index
-// cycle++
 function solution(s)
 {
-    let stack = []
-    if(s.length % 2 != 0) return 0;
+    let stack = [];
+    // 길이가 홀수이면 불가능
+    if (s.length % 2 !== 0) return 0;
     for (let i = 0; i < s.length; i++){
-        if (stack[stack.length - 1] === s[i]){
+        if (stack[stack.length -1] === s[i]){
             stack.pop()
         } else {
             stack.push(s[i])
         }
     }
-    return !stack.length ? 1 : 0
+    return stack.length === 0 ? 1 : 0
 }
