4b84d22ac73c3d16ae4d88292cece935c75f4708
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-07-11 13:26:59 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,12 +1,13 @@
+// 조건 1. n의 다음 큰 숫자는 n보다 큰 자연수 입니다.
+// 조건 2. n의 다음 큰 숫자와 n은 2진수로 변환했을 때 1의 갯수가 같습니다.
+// 조건 3. n의 다음 큰 숫자는 조건 1, 2를 만족하는 수 중 가장 작은 수 입니다.
 function solution(n) {
     let result = n+1;
-    let binaryN = n.toString(2).split('').filter((v)=> v === '1').length;
-    while(n < result) {
-        let binaryResult = result.toString(2).split('').filter((v)=> v === '1').length;
-        if (binaryN === binaryResult){
-            break
-        }
+    let binary = n.toString(2).split("").filter((v) => v === "1").length;
+    while(n < result){
+        let binaryResult = result.toString(2).split("").filter((v) => v === "1").length;
+        if (binary === binaryResult) break
         result++;
     }
-    return result
+    return result;
 }
