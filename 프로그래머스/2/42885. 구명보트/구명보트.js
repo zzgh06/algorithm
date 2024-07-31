de326496921d1bf66db309c56ae1ef3f1cc0b424
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-07-31 10:53:53 +0900

    [level 2] Title: 구명보트, Time: 28.81 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,10 +1,10 @@
-// 투포인터 알고리즘
 function solution(people, limit) {
-    people.sort((a, b)=> a - b);
+    people.sort((a, b) => a - b);
     let left = 0;
     let right = people.length - 1;
     let count = 0;
-    while(left <= right){
+    // 40 50 50 50 / 100
+    while(left <= right) {
         if (people[left] + people[right] <= limit){
             left++;
         }
