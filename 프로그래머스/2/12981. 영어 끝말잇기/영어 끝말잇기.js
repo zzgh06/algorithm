a444df08217f7c66df496276787d5dc58cbcb20c
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-07-31 11:49:13 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,16 +1,14 @@
 function solution(n, words) {
     let answer = [0, 0];
     let stack = [];
-    for (let i = 0; i < words.length; i++) {
+    for (let i = 0; i < words.length; i++){
         if (i > 0 && (stack.includes(words[i]) 
             || stack.at(-1).at(-1) !== words[i][0])) {
-            // 현재 단어가 이미 사용되었거나, 이전 단어의 마지막 문자와 일치하지 않으면 탈락
             let play = Math.floor(i / n) + 1;
             let person = (i % n) + 1;
             return [person, play];
         }
         stack.push(words[i]);
     }
-    
     return answer;
 }
