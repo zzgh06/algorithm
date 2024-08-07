3b34a58f3e07a04d09815f1ecdbf045506492cc0
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-08-07 13:00:24 +0900

    [level 2] Title: H-Index, Time: 0.40 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -0,0 +1,15 @@
+// H-지수 구하는 방법
+// 1. 자신이 저널에 등재한 전체 논문중 많이 인용된 순으로 정렬한 후, 
+// 2. 인용수가 논문수와 같아지거나
+// 3. 피인용수가 논문수보다 작아지기 시작하는 숫자
+
+function solution(citations) {
+    var answer = 0;
+    citations.sort((a, b) => b - a);
+    for (let i = 0; i < citations.length; i++){
+        if (citations[i] > i) {
+            answer++;
+        }
+    }
+    return answer;
+}
