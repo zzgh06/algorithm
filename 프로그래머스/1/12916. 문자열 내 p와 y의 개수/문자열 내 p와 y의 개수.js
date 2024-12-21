902aeeb67c90567aafb745568d12b4cc4f4f41a8
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-21 22:35:04 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.06 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,10 +1,7 @@
-// 문자열 s에서 p와 y의 갯수를 비교해서 같으면 true 틀리면 false 반환
-// 대소문자로 이루어져 있기 때문에 모두 대문자 혹은 소문자로 변환
-// 그리고 p와 y의 갯수를 비교 => split 활용 : 특정 문자를 기준으로 자르기 때문에 몇개인지 알 수 잇음
 function solution(s){
+    var answer = true;
+    let P = s.toUpperCase().split('P').length - 1;
+    let Y = s.toUpperCase().split('Y').length - 1;
 
-    let plength = s.toUpperCase().split('P').length - 1;
-    let Ylength = s.toUpperCase().split('Y').length - 1;
-
-    return plength == Ylength ? true : false;
+    return P === Y ? true : false
 }
