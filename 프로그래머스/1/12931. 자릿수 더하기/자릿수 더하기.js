cea980795555575af093233a4ac0fc6c4a4d6798
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-21 21:26:29 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,6 @@
-function solution(n){
-    let ans = 0
-    let 문자변환 = String(n)
-    for (let i = 0; i < 문자변환.length; i++) {
-        ans += parseInt(문자변환[i])
-    }
-    return ans
+function solution(n)
+{
+    return n.toString().split("").map(ele => {
+        return Number(ele)
+    }).reduce((a,b) => a += b, 0)
 }
