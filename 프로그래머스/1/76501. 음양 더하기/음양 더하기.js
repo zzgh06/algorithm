e2f56fbb59866e8daabd2d9deb5aafbc91c17678
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-21 22:57:44 +0900

    [level 1] Title: 음양 더하기, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,5 +1,3 @@
-// absolutes 절대값을 담은 배열 / signs 부호를 담은 boolean 배열
 function solution(absolutes, signs) {
-    let nums = absolutes.map((num, i)=>signs[i] === true ? num : -(num))
-    return nums.reduce((prev, cur)=> prev += cur, 0)
+    return absolutes.map((e, i) => signs[i] === true ? e : -(e)).reduce((a,b) => a += b, 0);
 }
