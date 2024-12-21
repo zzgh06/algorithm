85e2a300ed0ab8a44459ad51a5ca2e4cb0b9ac1e
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-21 22:43:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.08 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,5 @@
 function solution(x) {
-    const sum = String(x).split("").map(str=>Number(str)).reduce((acc,val)=>acc+val,0)
-    const isHarshad = Number.isInteger(x / sum)
-    return isHarshad;
+    const sum = String(x).split("").map(str=>Number(str)).reduce((acc,val)=>acc+val,0);
+    const 햐사드 = Number.isInteger(x / sum)
+    return 햐사드;
 }
