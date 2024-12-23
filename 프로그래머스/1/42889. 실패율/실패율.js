9934e78866e5f7f2f94cdda28e9716fc18f53cbc
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-23 20:41:36 +0900

    [level 1] Title: 실패율, Time: 5462.17 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,29 +1,12 @@
-// 1. hashMap 생성
-// 2. 1스테이지부터 n스테이지까지 순회
-// 3. 스테이지 통과한 사람 구하기
-// 4. 스테이지 통과하지 못한 사람 구하기
-// 5. 실패율 구하기
-// 6. 해당 스테이지와 실패율을 key value 로 저장
-// 7. 실패율에 따른 내림차순 정렬
-// 8. 내림차순 정렬된 key 값만 출력
+// 실패율 : 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+// 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return
 function solution(N, stages) {
     let answer = [];
-    let hashmap = new Map();
-    for (let i = 1; i <= N; i++){
-        let clear = 0;
-        let fail = 0;
-        let failureRate = 0;
-        for (let j = 0; j < stages.length; j++){
-            if (stages[j] >= i) {
-                clear++;
-            }
-            if (stages[j] === i) {
-                fail++;
-            }
-        }
-        failureRate = fail / clear;
-        hashmap.set(i, failureRate)
+    for (let i = 1; i <= N; i++) {
+        let clear = stages.filter(x => x >= i).length;
+        let fail = stages.filter(x => x === i).length;
+        answer.push([i, fail / clear])
     }
-    let hashmapSort = [...hashmap].sort((a, b)=>b[1] - a[1]);
-    return hashmapSort.map(hash => hash[0]);
+    answer.sort((a,b) => b[1] - a[1])
+    return answer.map(ele => ele[0]);
 }
