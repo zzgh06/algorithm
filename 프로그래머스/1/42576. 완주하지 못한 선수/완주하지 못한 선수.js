776da465cc5d6c4264a322a22efa27c1f5f8869d
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-23 21:24:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.85 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,20 @@
-// completion의 길이는 participant의 길이보다 1 작으므로
-// participant 배열의 값을 key로 value를 인원수로 해서 hash에 넣고
-// hash에서 completion의 배열을 key 값으로 이용해서 일치할 경우 -1
 function solution(participant, completion) {
     let hashMap = new Map();
-    participant.forEach((participant)=>{
-        if (!hashMap.get(participant)){
-            hashMap.set(participant, 1)
+    participant.forEach(part => {
+        if (!hashMap.get(part)) {
+            hashMap.set(part, 1)
         } else {
-            hashMap.set(participant, hashMap.get(participant) + 1)
+            hashMap.set(part, hashMap.get(part) + 1)
         }
     })
-    completion.forEach((completion)=>{
-       if (hashMap.get(completion)){
-           hashMap.set(completion, hashMap.get(completion) - 1)
-       }
+    
+    completion.forEach(comp => {
+        if (hashMap.get(comp)) {
+            hashMap.set(comp, hashMap.get(comp) - 1)
+        } 
     })
+    
     for (let [key, value] of hashMap) {
-        if (value > 0) {
-            return key
-        }
+        if (value > 0) return key
     }
 }
