6bf6369d8c15ca966e7b1e58d7358e487e063a68
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-23 20:29:44 +0900

    [level 1] Title: 과일 장수, Time: 219.01 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,12 +1,12 @@
+// 사과는 상태에 따라 1점부터 k점까지
+// 한 상자에 사과를 m개씩 담아 포장 / 상자에 담긴 사과 중 가장 낮은 점수가 p점
+// 사과 한 상자의 가격은 p * m 입니다.
+// 최대 이익을 계산
 function solution(k, m, score) {
-    let result = 0
-    score = score.sort((a,b)=>b-a);
-    // k = 3, m = 4
-    // console.log(score) [ 3, 3, 2, 2, 1, 1, 1 ]
-    //     
-    // score.length = 6
-    for(let i = m; i <= score.length; i += m){
-        result += score[i-1] * m
+    let answer = 0;
+    score.sort((a, b) => b - a); // [4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 1, 1]
+    for (let i = m; i <= score.length; i += m) {
+        answer += score[i - 1] * m
     }
-    return result
+    return answer;
 }
