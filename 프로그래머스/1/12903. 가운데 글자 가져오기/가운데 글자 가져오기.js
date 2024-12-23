f69f9a6800464a822b08c78054642bcac64b0f62
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-23 15:54:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,9 +1,7 @@
 function solution(s) {
-    var answer = '';
-    let index = 0
-    if (s.length % 2 === 0) {
-        return s.substr(Math.floor(s.length / 2) - 1, 2)
-    } else {
+    if (s.length % 2 === 1) {
         return s[Math.floor(s.length / 2)]
+    } else {
+        return s[Math.floor(s.length / 2) - 1] + s[Math.floor(s.length / 2)]
     }
 }
