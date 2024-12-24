9bd3f92113f0cb006a7c96f8767bf82cd51cbeff
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-24 14:18:53 +0900

    [level 1] Title: 폰켓몬, Time: 0.61 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,5 @@
-// 총 N 마리의 폰켓몬 중에서 N/2마리
 function solution(nums) {
     let limit = nums.length / 2
-    
-    return [...new Set(nums)].length > limit ? limit : [...new Set(nums)].length;
+    let pocketMon = [...new Set(nums)]
+    return pocketMon.length > limit ? limit : pocketMon.length;
 }
