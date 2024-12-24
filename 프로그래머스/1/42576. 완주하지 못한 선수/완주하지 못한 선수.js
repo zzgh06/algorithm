7ace655f734c2ffbb24ccdd5863d3e2472552f5e
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-24 14:24:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 36.51 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,18 @@
 function solution(participant, completion) {
     let hashMap = new Map();
-    participant.forEach(part => {
-        if (!hashMap.get(part)) {
-            hashMap.set(part, 1)
+    
+    participant.forEach((ele) => {
+        if (!hashMap.get(ele)) {
+            hashMap.set(ele, 1)
         } else {
-            hashMap.set(part, hashMap.get(part) + 1)
+            hashMap.set(ele, hashMap.get(ele) + 1)
         }
     })
     
-    completion.forEach(comp => {
-        if (hashMap.get(comp)) {
-            hashMap.set(comp, hashMap.get(comp) - 1)
-        } 
+    completion.forEach((ele) => {
+        if (hashMap.get(ele)) {
+            hashMap.set(ele, hashMap.get(ele) - 1)   
+        }
     })
     
     for (let [key, value] of hashMap) {
