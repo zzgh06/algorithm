c2af8c238f8a48dbf9cbad37bcd5b6798657ec3e
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-24 13:10:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 226.53 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,18 +1,15 @@
-// 빵-야체-고기-빵
-// 1-2-3-1
+// 스택활용(후입 선출) 조리된 순서대로 상수의 앞에 아래서부터 위로 쌓이게 되고
+// 빵 – 야채 – 고기 - 빵 = [1, 2, 3, 1]
+// [2, 1, 1, 2, 3, 1, 2, 3, 1]
 function solution(ingredient) {
     let answer = 0;
     let stack = [];
-    for (let i of ingredient){
-        // console.log(i)
+    for (let i of ingredient) {
         stack.push(i)
-        // 햄버거 재료가 4개 이상일 때, 4개씩 잘라서 1231경우만 stack에서 제거
-        if (stack.length >= 4){
-            if (stack.slice(-4).join("") === '1231'){
-                stack.splice(-4)
-                answer++
-            }
+        if (stack.slice(-4).join('') === '1231') {
+            stack.splice(-4)
+            answer++
         }
     }
-    return answer
+    return answer;
 }
