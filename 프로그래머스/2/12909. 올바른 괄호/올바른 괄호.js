0ba8fb04aebb256180a2c7fd3fa1e0b477b4ab2a
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-24 16:22:26 +0900

    [level 2] Title: 올바른 괄호, Time: 29.21 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,15 @@
 function solution(s){
-    let stack = []
-    for (let i = 0; i < s.length; i++){
-        if (s[0] === ')'){
+    let stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[0] === ")") {
             return false
-        } 
-        if (s[i] === '('){
+        }
+        if (s[i] === "(") {
             stack.push(s[i])
-        } else {
-            stack.pop()
+        } else if (s[i] === ")") {
+            stack.pop(s[i])
         }
     }
-    return stack.length === 0 ? true : false
+    return stack.length > 0 ? false : true
 }
