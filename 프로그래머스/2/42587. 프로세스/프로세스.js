9e3d989b0f8a64c688c5db2e0be9ca6b87709a32
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-24 16:35:57 +0900

    [level 2] Title: 프로세스, Time: 0.56 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,18 +1,18 @@
+// [A, B, C, D]
+// [2, 1, 3, 2]
 function solution(priorities, location) {
     // 큐 초기화 : 프로세스의 우선순위와 인덱스 객체로 저장
     const queue = priorities.map((priority, index) => ({ index, priority }));
+    // console.log(queue) [{ index: 0, priority: 2 }, { index: 1, priority: 1 }, ...]
     let executionOrder = 0;
-    
-    while(queue.length){
-        const current = queue.shift();
-        // 큐에 우선순위가 높은 작업이 있을 경우 큐 맨 뒤로 보낸다.
-        if (queue.some((item) => item.priority > current.priority)){
+    while(queue.length) {
+        let current = queue.shift()
+        
+        if (queue.some((x) => x.priority > current.priority)) {
             queue.push(current)
         } else {
-            // 그렇지 않으면 프로세스를 실행하고, 실행 순서를 증가
             executionOrder++;
-            // 현재 프로세스가 location 가 일치할 경우 executionOrder 반환
-            if (current.index === location) return executionOrder;
+            if (current.index === location) return executionOrder
         }
     }
     return executionOrder;
