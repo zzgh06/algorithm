7c0ce1b25111314bba7dec96ddef3603363dfc60
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-26 23:29:11 +0900

    [level 2] Title: H-Index, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -2,16 +2,13 @@
 // 1. 자신이 저널에 등재한 전체 논문중 많이 인용된 순으로 정렬한 후, 
 // 2. 인용수가 논문수와 같아지거나
 // 3. 피인용수가 논문수보다 작아지기 시작하는 숫자
-
 function solution(citations) {
-    let ans = 0
+    let res = 0
     citations.sort((a, b) => b - a)
-    // console.log(citations) [ 6, 5, 3, 1, 0 ]
-    for (let i = 0; i < citations.length; i++){
-        console.log(citations[i], i + 1)
-        if (citations[i] >= i + 1) {
-            ans++;
+    for (let i = 0; i < citations.length; i++) {
+        if (citations[i] > i) {
+            res++;
         }
     }
-    return ans
+    return res
 }
