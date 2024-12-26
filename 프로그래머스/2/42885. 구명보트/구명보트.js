a26fa4d4845f875f0d4251871c0de99f6c8a3206
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-26 14:46:16 +0900

    [level 2] Title: 구명보트, Time: 33.33 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,15 +1,14 @@
 function solution(people, limit) {
-    people.sort((a, b) => a - b);
+    people.sort((a, b)=> a - b);
     let left = 0;
     let right = people.length - 1;
     let count = 0;
-    // 40 50 50 50 / 100
     while(left <= right) {
-        if (people[left] + people[right] <= limit){
-            left++;
+        if(people[left] + people[right] <= limit) {
+            left++
         }
-        right--;
-        count++;
+        right--
+        count++
     }
     return count
 }
