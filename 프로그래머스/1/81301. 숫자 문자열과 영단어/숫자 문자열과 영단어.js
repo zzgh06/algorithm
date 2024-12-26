a03d078497e6b2830be0b07b24b94f7bbb50ecd9
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-26 20:02:04 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,6 +1,9 @@
+// s가 의미하는 원래 숫자를 return
 function solution(s) {
-
+    // 영단어가 담기 배열을 만들고
     let words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+    // 이를 순회하면서 숫자 문자열을 word로 구분하여 idx로 연결합니다
     words.forEach((word, idx) => {
         s = s.split(word).join(idx)
     })
