5cdfa3bd005a7bb547204cdf1b589609ea0754ad
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-26 23:14:39 +0900

    [level 2] Title: 전화번호 목록, Time: 154.49 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,8 +1,7 @@
 function solution(phone_book) {
-    phone_book.sort();
-    
-    for(let i = 0; i < phone_book.length - 1; i++){
-        if (phone_book[i] === phone_book[i + 1].substr(0, phone_book[i].length)){
+    phone_book = phone_book.sort()
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        if (phone_book[i] === phone_book[i + 1].slice(0, phone_book[i].length)) {
             return false
         }
     }
