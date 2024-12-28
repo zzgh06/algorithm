196050f95857a6c30a5eba5e419c650dd8b43566
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-28 23:31:06 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 48.65 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,9 +1,7 @@
-// 단순히 a 와 b 사이의 정수의 합을 구하는 문제가 아니라
-// a와 b의 대소관계가 정해져 있지 않으므로 이를 주의할 것
 function solution(a, b) {
-    let answer = 0;
+    let sum = 0
     for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
-        answer += i
+        sum += i
     }
-    return answer;
+    return sum
 }
