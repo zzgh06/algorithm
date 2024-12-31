ad5dd68abd28ae382b779c6b486d3b13a6db759a
chunghyun <121420299+zzgh06@users.noreply.github.com>
2024-12-31 13:10:41 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,9 +1,16 @@
+// array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬했을 때, k번째에 있는 수
+
 function solution(array, commands) {
-    let ans = []
-    commands.map((command) => {
-        let ele = array.slice(command[0] -1, command[1]).sort((a, b) => a - b)
-        // console.log(ele)
-        ans.push(ele[command[2] - 1])
+    let res = []
+    commands.forEach((command) => {
+        // [1, 5, 2, 6, 3, 7, 4]
+        // 0 1 2 3 4 5
+        // 1 2 3 4 5 6
+        
+        let arr = array.slice(command[0] - 1, command[1])
+        // console.log(arr)
+        arr.sort((a, b) => a - b)
+        res.push(arr[command[2] - 1])
     })
-    return ans
+    return res
 }
