e8409fd038c30c9fcbd3e6088483c8ef7107d2ff
chunghyun <121420299+zzgh06@users.noreply.github.com>
2025-01-06 19:38:15 +0900

    [level 1] Title: 카드 뭉치, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,15 +1,14 @@
-// cards1과 cards2에 적힌 단어들로 goal를 만들 있다면 "Yes"를, 만들 수 없다면 "No"를 return
-
 function solution(cards1, cards2, goal) {
     let cnt = 0;
-    for (let i = 0; i < goal.length; i++) {
-        if (cards1[0] === goal[i]) {
+    
+    goal.forEach((_, idx) => {
+        if (cards1[0] === goal[idx]) {
             cards1.shift()
             cnt++
-        } else if (cards2[0] === goal[i]) {
+        } else if (cards2[0] === goal[idx]) {
             cards2.shift()
             cnt++
         }
-    }
-    return goal.length === cnt ? 'Yes' : 'No';
+    })
+    return goal.length === cnt ? "Yes" : "No"
 }
